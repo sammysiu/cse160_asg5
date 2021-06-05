6e6ed145da1b541927b20d4db4e30ee4f64892b5
achoi15 <85264012+achoi15@users.noreply.github.com>
2021-06-05 01:27:44 -0700

    Add files via upload

--- asg5.js
+++ asg5.js
@@ -1,57 +1,123 @@
-const scene = new THREE.Scene();
+// Base code used for particles creation:
+// https://threejs.org/examples/webgl_points_waves.html
+const SEPARATION = 100, AMOUNTX = 50, AMOUNTY = 50;
 
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 1000);
-// const camera = new THREE.OrthographicCamera( -1, 1, 1, -1, 0.1, 1000 );
+let container;
+let camera, scene, renderer;
 
-// Creates a rendering context (similar to canvas.getContext(webgl))
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+let particles, count = 0;
 
-// Create camera controls
-const controls = new THREE.OrbitControls(camera, renderer.domElement);
-camera.position.z = 10;
-controls.update(); //controls.update() must be called after any manual changes to the camera's transform
+let windowHalfX = window.innerWidth / 2;
+let windowHalfY = window.innerHeight / 2;
 
-// Adds a canvas element with that context to the HTML body
-document.body.appendChild(renderer.domElement);
+init();
+animate();
 
-// Creates a cylinder
-// const geometry = new THREE.BoxGeometry();
-const geometry = new THREE.CylinderGeometry(1, 1, 5, 32 );
-// const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-const phongMaterial = new THREE.MeshPhongMaterial( { color: 0xffff00, shininess: 16 } );
-const cylinder = new THREE.Mesh(geometry, phongMaterial);
+function init() {
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
 
-// Examples of transformations
-cylinder.position.x = 1;
-cylinder.position.y = -1;
-cylinder.rotation.z = 45;
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    camera.position.set( 3000, 3000, 3000 );
 
-scene.add(cylinder);
+    scene = new THREE.Scene();
 
-// White directional light at half intensity shining from the top.
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 1.0 );
-directionalLight.target =
-scene.add(directionalLight);
+    const numParticles = AMOUNTX * AMOUNTY;
 
-// Creates a point light source
-const light1 = new THREE.PointLight( 0xffffff, 1, 100 );
-light1.position.set(-1, -1, -1);
-scene.add(light1);
+    const positions = new Float32Array( numParticles * 3 ); // 3 vertices per particle
+    const scales = new Float32Array( numParticles );
 
-// Creates a point light source
-const light2 = new THREE.PointLight( 0x00ff00, 1, 100 );
-light2.position.set(1, 1, 1);
-scene.add(light2);
 
-time = 0;
-function draw() {
-    requestAnimationFrame(draw);
+    let i = 0, j = 0;
+    // Calculate positions and scales of particles
+    for ( let ix = 0; ix < AMOUNTX; ix ++ ) {
 
-    // Examples of animation
-    cylinder.scale.y = Math.sin(time);
-    time += 0.01;
+        for ( let iy = 0; iy < AMOUNTY; iy ++ ) {
 
-    renderer.render(scene, camera);
+            positions[ i ] = ix * SEPARATION - ( ( AMOUNTX * SEPARATION ) / 2 ); // x
+            positions[ i + 1 ] = 0; // y
+            positions[ i + 2 ] = iy * SEPARATION - ( ( AMOUNTY * SEPARATION ) / 2 ); // z
+
+            scales[ j ] = 1;
+
+            i += 3;
+            j ++;
+
+        }
+
+    }
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+    geometry.setAttribute( 'scale', new THREE.BufferAttribute( scales, 1 ) );
+
+    const material = new THREE.ShaderMaterial( {
+        uniforms: {
+            color: { value: new THREE.Color( 0x00ead3 ) },
+            opacity:0.5,
+            blending:THREE.AdditiveBlending
+        },
+        vertexShader: document.getElementById( 'vertexshader' ).textContent,
+        fragmentShader: document.getElementById( 'fragmentshader' ).textContent
+
+    } );
+
+    particles = new THREE.Points( geometry, material );
+    scene.add( particles );
+
+    //
+
+    renderer = new THREE.WebGLRenderer( { antialias: true , preserveDrawingBuffer: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+    // const composer = new EffectComposer( renderer );
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.update();
+    // stats = new Stats();
+    // container.appendChild( stats.dom );
+
+    container.style.touchAction = 'none';
+
 }
-draw();
+
+function animate() {
+
+    requestAnimationFrame( animate );
+
+    render();
+    // stats.update();
+
+}
+
+function render() {
+    const positions = particles.geometry.attributes.position.array;
+    const scales = particles.geometry.attributes.scale.array;
+
+    let i = 0, j = 0;
+    // Update positions and scales of particles
+    for ( let ix = 0; ix < AMOUNTX; ix ++ ) {
+
+        for ( let iy = 0; iy < AMOUNTY; iy ++ ) {
+
+            positions[ i + 1 ] = ( Math.sin( ( ix + count ) * 0.3 ) * 50 ) +
+                            ( Math.sin( ( iy + count ) * 0.5 ) * 50 );
+
+            scales[ j ] = ( Math.sin( ( ix + count ) * 0.3 ) + 1 ) * 20 +
+                            ( Math.sin( ( iy + count ) * 0.5 ) + 1 ) * 20;
+
+            i += 3;
+            j ++;
+
+        }
+
+    }
+
+    particles.geometry.attributes.position.needsUpdate = true;
+    particles.geometry.attributes.scale.needsUpdate = true;
+
+    renderer.render( scene, camera );
+
+    count += 0.1;
+
+}
